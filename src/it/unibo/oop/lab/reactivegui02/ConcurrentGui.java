399c672868477aa66ddf4c4aeba26fb42e43419c
Luca Rapolla <luca.rapolla@studio.unibo.it>
2021-11-22 12:39:23 +0100

    Finish reactivegui02

--- src/it/unibo/oop/lab/reactivegui02/ConcurrentGui.java
+++ src/it/unibo/oop/lab/reactivegui02/ConcurrentGui.java
@@ -3,12 +3,17 @@ package it.unibo.oop.lab.reactivegui02;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.reflect.InvocationTargetException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
+import javax.swing.SwingUtilities;
+
 
 public class ConcurrentGui extends JFrame {
     
@@ -18,7 +23,7 @@ public class ConcurrentGui extends JFrame {
     private static final double WIDTH_PERC = 0.2;
     private static final double HEIGHT_PERC = 0.1;
     
-    private final JLabel display = new JLabel("ciao");
+    private final JLabel display = new JLabel();
     final JButton up = new JButton("up");
     final JButton down = new JButton("down");
     final JButton stop = new JButton("stop");
@@ -29,7 +34,7 @@ public class ConcurrentGui extends JFrame {
         
         super();
         final Dimension screensize = Toolkit.getDefaultToolkit().getScreenSize();
-        this.setSize((int) (screensize.getWidth() * WIDTH_PERC), (int) (screensize.getHeight() * HEIGHT_PERC));
+        this.setSize((int) (screensize.getWidth() * WIDTH_PERC ), (int) (screensize.getHeight() * HEIGHT_PERC));
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         
         final JPanel canvas = new JPanel();
@@ -41,18 +46,89 @@ public class ConcurrentGui extends JFrame {
         
         
         this.getContentPane().add(canvas);
-        this.pack();
+        
         this.setVisible(true);
         
         
+        final Agent agent = new Agent();
+        new Thread(agent).start();
         
         
+        stop.addActionListener(new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                agent.stopCounting();
+                up.setEnabled(false);
+                down.setEnabled(false);
+            }
+        });
         
-        
-        
-        
-        
-        
+        up.addActionListener(new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                agent.setForward();
+            }
+        });
+       
+        down.addActionListener(new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                agent.setBackward();
+            }
+        });
     }
+    
+    private class Agent implements Runnable{
+        
+        private volatile boolean stop;
+        private volatile int counter;
+        private boolean forward = true;
+        
+        @Override
+        public void run() {
+            while(!this.stop) {
+                try {
+                    SwingUtilities.invokeAndWait(new Runnable() {
+                        
+                        @Override
+                        public void run() {
+                            ConcurrentGui.this.display.setText(Integer.toString(Agent.this.counter));
+                            
+                        }
+                    });
+                    
+                    if(this.forward) {
+                        this.counter++;                        
+                    }else {
+                        this.counter--;
+                    }
+                    
+                    Thread.sleep(100);
+                }catch(InvocationTargetException | InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            
+        }
+        
+    
+    
+        public void stopCounting() {
+            this.stop = true;
+        }
+        
+        public void setForward() {
+            this.forward = true;
+        }
+        
+        public void setBackward() {
+            this.forward = false;
+        }
+    }
+    
+
     
 }
